16dbb1d9c77996b7e3e6239d301d76610ced54ad
exhyn <exhyn@protonmail.com>
2021-05-26 00:35:29 -0300

    div(1e18) missing?

--- src/utils/apy.ts
+++ src/utils/apy.ts
@@ -10,7 +10,7 @@ import BigNumber from 'bignumber.js';
 export const getFarmApy = (poolWeight: BigNumber, yogiPerBlock: BigNumber, yogiPrice: BigNumber, poolLiquidity: BigNumber): number => {
   const BLOCKS_PER_YEAR = parseInt(process.env.REACT_APP_BLOCKS_PER_YEAR);
   const yearlyRewards: BigNumber = yogiPerBlock.times(BLOCKS_PER_YEAR).times(poolWeight);
-  const apy: BigNumber = yearlyRewards.times(yogiPrice).div(poolLiquidity).times(100);
+  const apy: BigNumber = yearlyRewards.times(yogiPrice).div(poolLiquidity).times(100).div(1e18);
 
   return apy.isNaN() || !apy.isFinite() ? null : apy.toNumber();
 };
